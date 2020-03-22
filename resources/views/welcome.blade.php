cb1455142b609eb32e4873d74e17d929a1994efb
chien9292 <40844671+chien9292@users.noreply.github.com>
2020-03-22 11:29:41 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravellllll
+                    Laravel
                 </div>
 
                 <div class="links">
